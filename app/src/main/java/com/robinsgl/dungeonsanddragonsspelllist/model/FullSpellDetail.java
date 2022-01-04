9ab51cd6f0eceb5e0c1b2e097f0a7fef0cbc8e51
Georgia Robinson <gldrobinson@hotmail.co.uk>
2022-01-04 13:56:41 +0000

    creates class for FullSpellDetail

---
+++ app/src/main/java/com/robinsgl/dungeonsanddragonsspelllist/model/FullSpellDetail.java
@@ -0,0 +1,133 @@
+package com.robinsgl.dungeonsanddragonsspelllist.model;
+
+public class FullSpellDetail {
+    private String name;
+    private String level;
+    private String castingTime;
+    private String range;
+    private String duration;
+    private String attackSave;
+    private String damageEffect;
+    private String components;
+    private String school;
+    private String description;
+    private String higherLevel;
+    private String material;
+    private String ritual;
+    private String concentration;
+
+    public FullSpellDetail() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getCastingTime() {
+        return castingTime;
+    }
+
+    public void setCastingTime(String castingTime) {
+        this.castingTime = castingTime;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getAttackSave() {
+        return attackSave;
+    }
+
+    public void setAttackSave(String attackSave) {
+        this.attackSave = attackSave;
+    }
+
+    public String getDamageEffect() {
+        return damageEffect;
+    }
+
+    public void setDamageEffect(String damageEffect) {
+        this.damageEffect = damageEffect;
+    }
+
+    public String getComponents() {
+        return components;
+    }
+
+    public void setComponents(String components) {
+        this.components = components;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHigherLevel() {
+        return higherLevel;
+    }
+
+    public void setHigherLevel(String higherLevel) {
+        this.higherLevel = higherLevel;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getRitual() {
+        return ritual;
+    }
+
+    public void setRitual(String ritual) {
+        this.ritual = ritual;
+    }
+
+    public String getConcentration() {
+        return concentration;
+    }
+
+    public void setConcentration(String concentration) {
+        this.concentration = concentration;
+    }
+}

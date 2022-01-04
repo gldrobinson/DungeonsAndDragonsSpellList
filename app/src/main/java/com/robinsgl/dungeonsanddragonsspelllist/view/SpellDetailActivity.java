8237fc646301f44c14fd05895469e71acd1901de
Georgia Robinson <gldrobinson@hotmail.co.uk>
2022-01-04 19:41:56 +0000

    sets desciption, higher levels and materials through viewModel

--- app/src/main/java/com/robinsgl/dungeonsanddragonsspelllist/view/SpellDetailActivity.java
+++ app/src/main/java/com/robinsgl/dungeonsanddragonsspelllist/view/SpellDetailActivity.java
@@ -15,9 +15,14 @@ import com.robinsgl.dungeonsanddragonsspelllist.viewModel.SpellDetailViewModel;
 
 public class SpellDetailActivity extends AppCompatActivity {
 
+    SpellDetailViewModel spellDetailViewModel;
     TextView spellNameTextView;
-    View divisorView;
+    View divisorViewTop;
     GridView gridView;
+    View divisorViewBottom;
+    TextView materialTextView;
+    TextView spellDescriptionTextView;
+    TextView spellHigherLevelTextView;
 
 
     @Override
@@ -25,17 +30,21 @@ public class SpellDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spell_detail);
 
+        spellDetailViewModel = new SpellDetailViewModel();
+
         spellNameTextView = findViewById(R.id.spell_name_text_view);
-        divisorView = findViewById(R.id.divisor_view);
+        divisorViewTop = findViewById(R.id.divisor_view_top);
+        divisorViewBottom = findViewById(R.id.divisor_view_bottom);
         gridView = findViewById(R.id.grid_view);
+        materialTextView = findViewById(R.id.material_text_view);
+        spellDescriptionTextView = findViewById(R.id.spell_description_text_view);
+        spellHigherLevelTextView = findViewById(R.id.spell_higher_level_text_view);
 
         Intent intent = getIntent();
         String spellNameReceived = intent.getStringExtra("spellName");
         String spellUrlReceived = intent.getStringExtra("spellUrl");
 
         spellNameTextView.setText(spellNameReceived);
-
-        SpellDetailViewModel spellDetailViewModel = new SpellDetailViewModel();
 
         spellDetailViewModel.getSpellDetail(spellUrlReceived);
 
@@ -48,8 +57,21 @@ public class SpellDetailActivity extends AppCompatActivity {
         });
 
         spellDetailViewModel.fullSpellDetail.observe(this, fullSpellDetail -> {
+            String spellDescription = fullSpellDetail.getDescription();
+            String higherLevelString = "At Higher Levels. ";
+            String higherLevel = fullSpellDetail.getHigherLevel();
+            String materials = fullSpellDetail.getMaterial();
 
-                });
+            spellDescriptionTextView.setText(spellDescription);
+            spellHigherLevelTextView.setText(higherLevelString + higherLevel);
+
+            if (materials != null) {
+                materialTextView.setText("* " +materials);
+            } else {
+                materialTextView.setText("");
+            }
+            
+        });
 
     }
 }
